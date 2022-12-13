f995b57b9496884c1f05c967693e6f4f0dd632ed
Matouš Větrovec <90977968+Madrakon@users.noreply.github.com>
2022-12-13 20:47:31 +0100

    Add files via upload

---
+++ mv_interactions/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Interactions'
+
+version '1.0'
+
+client_scripts {
+    'client/config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    'client/config.lua',
+	'server/main.lua'
+}
